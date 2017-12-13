1d6b83e70611605afa4eb77b0b1d10c8034566e8
Sergio Wilson <wilsonsergio2500@gmail.com>
2017-12-13 09:58:56 -0600

    completed vanilla formly

--- angular-material2-extended/src/app/fomly-fields/Fields.ts
+++ angular-material2-extended/src/app/fomly-fields/Fields.ts
@@ -42,6 +42,12 @@ export namespace Fields {
     [additionalProperties: string]: any;
   }
 
+  interface IFormlyValidator{
+    expression: (formGroup : FormGroup) => boolean;
+    message: (error, field: FormlyFieldConfig) => string;
+
+  }
+
   class InputBase implements FormlyFieldConfig {
     key: string;
     type: string;
@@ -55,10 +61,10 @@ export namespace Fields {
       show?: boolean;
       [additionalProperties: string]: any;
     };
-    validators?: any;
+    validators?: { [validatorProperty: string] : IFormlyValidator } //any;
     asyncValidators?: any;
     formControl?: AbstractControl;
-
+     hideExpression?: boolean | string | ((model: any, formState: any) => boolean);
     className?: string;
 
     constructor(key: string, label: string, required: boolean = false) {
@@ -68,7 +74,7 @@ export namespace Fields {
 
       const messages = {
         required: (error, field: FormlyFieldConfig) => {
-          return field.templateOptions.label + ' is required';
+          return `${field.templateOptions.label} is required`;
         }
       }
 
@@ -93,15 +99,22 @@ export namespace Fields {
             super(key, label, required);
             this.validators = {
                 'email': {
-                    expression: ($viewvalue, $modelvalue) => {
-                        let value = $modelvalue || $viewvalue;
-                        let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-                        return regex.test(value);
+                    expression: (formGroup : FormGroup) => {
+                        let value = formGroup.value;
+                        if(!!value){
+                            let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                            return regex.test(value);
+                        } else {
+                          //if value should be handle by the required validator
+                          return true;
+                        }
+                        
                     },
-                    message: ($viewvalue, $modelvalue) => {
-                        return ($viewvalue) && $viewvalue + ' is not a valid Email Address' ;
+                    message: (error, field: FormlyFieldConfig) => {
+                        return `${field.formControl.value} is not a valid Email Address` ;
                     }
-                }
+                },
+             
 			  }
 		}
 	}
--- angular-material2-extended/src/app/views/root/dashboard/vanilla-formly/vanilla-formly.component.ts
+++ angular-material2-extended/src/app/views/root/dashboard/vanilla-formly/vanilla-formly.component.ts
@@ -1,11 +1,92 @@
 
 import { Component } from '@angular/core'
+import { FormGroup } from '@angular/forms';
+import { FormlyFormOptions, FormlyFieldConfig } from '@ngx-formly/core';
 
 @Component({
   selector: 'vanilla-formly',
   templateUrl: 'vanilla-formly.component.html'
 })
 export class VanillaFormlyComponent{
+  form: FormGroup;
+  model: any;
+  fields: FormlyFieldConfig[];
   constructor(){
+
+    this.form = new FormGroup({});
+    this.model = {};
+
+    const messages = {
+        required: (error, field: FormlyFieldConfig) => {
+          return field.templateOptions.label + ' is required';
+        }
+      }
+
+    this.fields = [
+      {
+        key: 'text',
+        type: 'input',
+        templateOptions: {
+          label: 'Text 1',
+          placeholder: 'enter an item',
+          required: true
+
+        },
+        validation: { messages }
+         
+      },
+      {
+        key: 'email',
+        type: 'input',
+        templateOptions: {
+          label: 'Email',
+          required: true
+
+        },
+        validation: { messages },
+        validators : {
+                //'required': {
+                //    expression: (formGroup : FormGroup) => {
+                //      return (!!formGroup.value);
+                //    },
+                //    message: (item: any, formlyConfig: FormlyFieldConfig) => {
+                      
+                //        return `${formlyConfig.templateOptions.label} is required` ;
+                //    }
+                //},
+                'email': {
+                   expression: (FormGroup: FormGroup) => {
+                        let value = FormGroup.value;
+                        if(!!value) {
+                          let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                          return regex.test(value);
+
+                          }
+                        return true;
+                   },
+                    message: (item: any, formlyConfig: FormlyFieldConfig) => {
+                      return `${formlyConfig.formControl.value} is not a valid Email Address` ;
+                  }
+                }
+			  }
+        
+      },
+      {
+        key: 'password',
+        type: 'input',
+        templateOptions: {
+          label: 'Password',
+          type: 'password',
+          required: true
+        },
+        validation: { messages },
+      }
+      ];
+
   }
+
+  submit() {
+    console.log(this.model);
+  }
+
 }
